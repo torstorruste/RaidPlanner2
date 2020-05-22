9ee6b8265f505de3b2c10382e0e87a6acd0ecf5a
Tor Storruste <tor@superhelt.org>
2020-05-22 20:49:29 +0200

    Now writes to a data folder rather than working directory

--- src/main/java/org/superhelt/raidplanner2/dao/FileWriter.java
+++ src/main/java/org/superhelt/raidplanner2/dao/FileWriter.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,33 +15,44 @@ import java.util.List;
 public class FileWriter {
 
     private static final Logger log = LoggerFactory.getLogger(FileWriter.class);
+    private static final Path dataFolder = getDataFolder();
 
-    public static <T> List<T> readFromFile(Path jsonFile, Class tClass) throws IOException {
-        if(Files.exists(jsonFile)) {
-            String json = readFile(jsonFile);
+    private static Path getDataFolder() {
+        String dataFolder = System.getenv("RAIDPLANNER_DATA_FOLDER");
+
+        return dataFolder == null ? Paths.get("") : Paths.get(dataFolder);
+    }
+
+    public static <T> List<T> readFromFile(Path jsonFile, Class<T[]> tClass) throws IOException {
+        Path file = dataFolder.resolve(jsonFile);
+
+        if (Files.exists(file)) {
+            String json = readFile(file);
 
             ObjectMapper mapper = new ObjectMapper();
             T[] storedObjects = mapper.readerFor(tClass).readValue(json);
 
             List<T> result = Arrays.asList(storedObjects);
-            log.info("Read {} objects of type {} from {}", result.size(), tClass.getSimpleName(), jsonFile);
+            log.info("Read {} objects of type {} from {}", result.size(), tClass.getSimpleName(), file);
 
             return result;
         } else {
-            log.info("Found no file to read ({})", jsonFile);
+            log.info("Found no file to read ({})", file);
             return new ArrayList<>();
         }
     }
 
     public static <T> void writeToFile(Path jsonFile, List<T> list) {
+        Path file = dataFolder.resolve(jsonFile);
+
         try {
             ObjectMapper mapper = new ObjectMapper();
             String json = mapper.writeValueAsString(list);
 
-            Files.deleteIfExists(jsonFile);
-            Files.write(jsonFile, json.getBytes());
-        } catch(Exception e) {
-            log.error("Unable to write file {}", jsonFile, e);
+            Files.deleteIfExists(file);
+            Files.write(file, json.getBytes());
+        } catch (Exception e) {
+            log.error("Unable to write file {}", file, e);
         }
     }
 
