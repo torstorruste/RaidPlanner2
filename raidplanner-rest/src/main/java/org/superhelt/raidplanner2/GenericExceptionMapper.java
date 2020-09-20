91853d2b63f1a8c97dea650694678240cac0452e
Tor Storruste <tor@superhelt.org>
2020-09-20 15:49:32 +0200

    Added cors filter, and treats NotFound as 404

--- raidplanner-rest/src/main/java/org/superhelt/raidplanner2/GenericExceptionMapper.java
+++ raidplanner-rest/src/main/java/org/superhelt/raidplanner2/GenericExceptionMapper.java
@@ -3,6 +3,7 @@ package org.superhelt.raidplanner2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
@@ -17,6 +18,8 @@ public class GenericExceptionMapper implements ExceptionMapper<Exception> {
         ServerException ex;
         if(exception instanceof ServerException) {
             ex = (ServerException) exception;
+        } else if(exception instanceof NotFoundException) {
+            return Response.status(404).entity(exception.getMessage()).build();
         } else {
             log.error("Unexpected exception encountered in exception mapper.", exception);
             ex = new ServerException(exception.getMessage());
--- raidplanner-rest/src/main/java/org/superhelt/raidplanner2/MyApplication.java
+++ raidplanner-rest/src/main/java/org/superhelt/raidplanner2/MyApplication.java
@@ -1,6 +1,7 @@
 package org.superhelt.raidplanner2;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.superhelt.raidplanner2.filter.CorsFilter;
 
 import javax.ws.rs.ApplicationPath;
 
@@ -9,6 +10,7 @@ public class MyApplication extends ResourceConfig {
     public MyApplication() {
         register(new RaidPlannerBinder());
         register(GenericExceptionMapper.class);
+        register(CorsFilter.class);
         packages("org.superhelt.raidplanner2");
     }
 }
